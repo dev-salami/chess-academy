8fb0f990bbaa0b87d2902f5c7f3b3bbd67e37f7b
Salami Khalil <salamikhalil02@gmail.com>
2024-05-27 13:05:49 +0100

    content update

--- src/components/home/Tutors.jsx
+++ src/components/home/Tutors.jsx
@@ -7,7 +7,7 @@ function Tutors() {
   return (
     <section className="pt-32" id="tutors">
       <p className="text-4xl font-semibold mb-10  text-center">
-        Meet Our Tutors
+        Meet Our Team Leaders
       </p>
       <main className="grid grid-cols-1 gap-x-6 gap-y-16 px-8 md:grid-cols-2">
         {tutors.map((tutor, index) => (

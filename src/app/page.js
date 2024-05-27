d84a7e08c101a49763cbb794acbcde86855f1f7b
Salami Khalil <salamikhalil02@gmail.com>
2024-05-27 13:03:55 +0100

    removed lenis scroll

--- src/app/page.js
+++ src/app/page.js
@@ -8,32 +8,32 @@ import AboutUs from "@/components/home/About-Us";
 import Testimonial from "@/components/home/Testimonial";
 import Footer from "@/components/Footer";
 import Courses from "@/components/home/Courses";
-import Lenis from "@studio-freight/lenis";
-import { useEffect } from "react";
+// import Lenis from "@studio-freight/lenis";
+// import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    const lenis = new Lenis();
+  // useEffect(() => {
+  //   const lenis = new Lenis();
 
-    // Check if the `window` object is available before using it
-    if (typeof window !== "undefined") {
-      lenis.on("scroll", (e) => {
-        // console.log(e);
-      });
+  //   // Check if the `window` object is available before using it
+  //   if (typeof window !== "undefined") {
+  //     lenis.on("scroll", (e) => {
+  //       // console.log(e);
+  //     });
 
-      function raf(time) {
-        lenis.raf(time);
-        requestAnimationFrame(raf);
-      }
+  //     function raf(time) {
+  //       lenis.raf(time);
+  //       requestAnimationFrame(raf);
+  //     }
 
-      requestAnimationFrame(raf);
-    }
+  //     requestAnimationFrame(raf);
+  //   }
 
-    // Clean up the Lenis instance when the component unmounts
-    return () => {
-      lenis.destroy();
-    };
-  }, []);
+  //   // Clean up the Lenis instance when the component unmounts
+  //   return () => {
+  //     lenis.destroy();
+  //   };
+  // }, []);
   return (
     <div>
       <Navbar />

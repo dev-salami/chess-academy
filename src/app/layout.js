f86bbe2492cdd1223321bae843e24da544fda126
Salami Khalil <salamikhalil02@gmail.com>
2024-07-07 16:30:32 +0100

    seo update

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,22 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Moving Train",
-  description: "Moving Train Chess Academy",
+  metadataBase: new URL("https://www.themovingtrain.org"),
+  title: {
+    default: "Moving Train | Moving Train Chess Academy",
+    template: "%s | Moving Train",
+  },
+  description:
+    "The Moving Train Online Academy offers top-notch chess education, fostering lifelong passion and skills. Our expert instruction cultivates strategic thinking, problem-solving, and sportsmanship to prepare players for success in chess and life.",
+  openGraph: {
+    title: "Moving Train | Moving Train Chess Academy",
+    description:
+      "The Moving Train Online Academy offers top-notch chess education, fostering lifelong passion and skills. Our expert instruction cultivates strategic thinking, problem-solving, and sportsmanship to prepare players for success in chess and life.",
+    type: "website",
+    locale: "en-US",
+    url: "https://www.themovingtrain.org/",
+    siteName: "Moving Train",
+  },
 };
 
 export default function RootLayout({ children }) {

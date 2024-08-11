5dcfca4d1f5560962665e54662f975fbe339ceba
Salami Khalil <salamikhalil02@gmail.com>
2024-08-11 23:51:34 +0100

    update: location

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,13 +6,13 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   metadataBase: new URL("https://www.themovingtrain.org"),
   title: {
-    default: "Moving Train | Moving Train Chess Academy",
+    default: "Moving Train Nigeria | Moving Train Chess Academy",
     template: "%s | Moving Train",
   },
   description:
     "The Moving Train Online Academy offers top-notch chess education, fostering lifelong passion and skills. Our expert instruction cultivates strategic thinking, problem-solving, and sportsmanship to prepare players for success in chess and life.",
   openGraph: {
-    title: "Moving Train | Moving Train Chess Academy",
+    title: "Moving Train Nigeria | Moving Train Chess Academy",
     description:
       "The Moving Train Online Academy offers top-notch chess education, fostering lifelong passion and skills. Our expert instruction cultivates strategic thinking, problem-solving, and sportsmanship to prepare players for success in chess and life.",
     type: "website",

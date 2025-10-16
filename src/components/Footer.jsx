83a26f7a3bb43f9f8ee6571ad917546a72ca2111
donfyla <alabioyetope@gmail.com>
2025-10-16 20:27:31 +0100

    put icons just below the footer logo instead of bottom of page.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -23,7 +23,41 @@ function Footer() {
         <div className="grid gap-6 md:grid-cols-4 mb-12 ">
           <div className="flex flex-col gap-6 justify-center items-center">
             <Image alt="Logo" src={logo} height={100} width={100}></Image>
-            <div>              
+            <div>
+              <div className="gap-4 my-2 flex justify-center">
+                <a
+                  href="https://www.instagram.com/movingtchess?igsh=MTg2NTkwc2tseW5oOA=="
+                  className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaSquareInstagram size={25} />
+                </a>
+                <a
+                  href="https://www.facebook.com/your-facebook-page"
+                  className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaFacebook size={25} />
+                </a>
+                <a
+                  href="https://www.linkedin.com/your-linkedin-profile"
+                  className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <FaLinkedin size={25} />
+                </a>
+                <a
+                  href="https://www.facebook.com/your-facebook-page"
+                  className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <RiFacebookCircleLine size={25} />
+                </a>
+              </div>
               <div className="flex text-center">
                 <span>TEL : </span>
                 <p>+2348084969784</p>
@@ -42,40 +76,7 @@ function Footer() {
             <a href="#event"> Contact</a>
           </div>
           <div>
-            <div className="gap-4 my-2 flex justify-center">
-              <a
-                href="https://www.instagram.com/movingtchess?igsh=MTg2NTkwc2tseW5oOA=="
-                className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaSquareInstagram size={25} />
-              </a>
-              <a
-                href="https://www.facebook.com/your-facebook-page"
-                className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaFacebook size={25} />
-              </a>
-              <a
-                href="https://www.linkedin.com/your-linkedin-profile"
-                className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <FaLinkedin size={25} />
-              </a>
-              <a
-                href="https://www.facebook.com/your-facebook-page"
-                className="flex rounded-full bg-white items-center justify-center text-[#0B0804]"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <RiFacebookCircleLine size={25} />
-              </a>
-            </div>
+
           </div>
         </div>
       </div>
